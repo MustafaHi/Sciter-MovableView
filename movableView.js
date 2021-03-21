2b35ceee7585bf736c5c5cb6f99aec299797cf00
MustafaHi <matterik971@gmail.com>
2021-03-21 19:04:52 +0300

    EXPERIMENTAL JS PORT 02
    - Screen bound
    - Now it's usable inside uSciter and standard windows
    - Will be fully functional by next Sciter.JS release
    - Number class extended by `limit()` function, like `Integer.limit()` in Sciter.TIS

--- movableView.js
+++ movableView.js
@@ -1,15 +1,42 @@
-// Movable View v1.2 EXPERIMENTAL JS PORT 01
+// Movable View v1.2 EXPERIMENTAL JS PORT 02
 // https://github.com/MustafaHi/Sciter-MovableView
 
 // Call movableView('selector' [, screenBound:bool(false)]);
+
+Number.prototype.limit = function(min,max) {
+  if (this < min) return min;
+  if (this > max) return max;
+  return this;
+}
+
 function movableView(s, screenBound = false)
 {
   var xoff,yoff, minXY, maxX, maxY;
   var dragging = false;
 
+  function screenBounds()
+  {
+    if (screenBound)
+    {
+      [maxX, maxY] = Window.this.screenBox("workarea", "dimension");
+      var [w, h]   = Window.this.box("dimension", "border");
+      maxX -= w;
+      maxY -= h;
+      minXY = 0;
+    }
+    else
+    {
+      maxX = Number.MAX_SAFE_INTEGER;
+      maxY = Number.MAX_SAFE_INTEGER;
+      minXY = Number.MIN_SAFE_INTEGER;
+    }
+  }
+
   function onMouseDown(e)
   {
-    xoff = e.clientX; yoff = e.clientY;
+    screenBounds();
+    var [x,y] = Window.this.box("position", "border", "screen");
+    xoff = e.screenX - x; yoff = e.screenY - y;
     dragging = true;
 
     document.on("mouseup"  , onMouseUp   );
@@ -23,7 +50,7 @@ function movableView(s, screenBound = false)
     if(dragging)
     {
       e.preventDefault();
-      Window.this.move(e.screenX - xoff, e.screenY - yoff);
+      Window.this.move((e.screenX - xoff).limit(minXY, maxX), (e.screenY - yoff).limit(minXY, maxY));
     }
   }
 
